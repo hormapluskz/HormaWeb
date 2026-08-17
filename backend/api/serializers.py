0a7497ab5477a29d4212d6d05e4fc4849daf5556
aba111f <karata93@mail.ru>
2026-08-17 17:21:53 +0500

    serializer changed

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -5,5 +5,5 @@ from .models import ContactMessage
 class ContactMessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = ContactMessage
-        fields = ['id', 'name', 'email', 'message', 'created_at']
+        fields = ['id', 'name', 'email', 'phone', 'message', 'created_at']
         read_only_fields = ['id', 'created_at']
